9ecdc04e503e4445375ced58b418667756d7393a
Oga-Emma <sevenapps2018@gmail.com>
2018-07-01 23:09:00 +0100

    Bug fixes

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # JournalApplication
 Google ALC 3.0 Challenge #2
 
+##Application description
+This is a Journal Application to help users write down journal entries on their mobile.
+Journal are saved online or locally, depending on the user preferences
+
 ## Features
 - google signin
 - offline mode
 
-Link to Apk = (http://link.com)
+[Link to Apk](https://drive.google.com/file/d/1T5jMsVUDa1kgEL9ED5GRopqL2vcPub_-/view?usp=sharing)
 
 
 
---
+++ app/src/test/java/ng/com/oga_emma/journalapplication/database/JournalEntryFirebaseDBTest.java
@@ -0,0 +1,32 @@
+package ng.com.oga_emma.journalapplication.database;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JournalEntryFirebaseDBTest {
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void fetchJournalEntries() {
+    }
+
+    @Test
+    public void addJournalEntry() {
+    }
+
+    @Test
+    public void updateJournalEntry() {
+    }
+
+    @Test
+    public void deleteJournalEntry() {
+    }
+
+    @Test
+    public void clearListeners() {
+    }
+}
